f7d1d5ed60c6ee9d5c22f4553c2e50b2df5a9558
Goncalo Mendes <goncalo.mendes@altran.com>
2022-02-17 20:08:06 +0100

    Implement correct handling of Post method

--- SynetecAssessmentApi/Services/BonusPoolService.cs
+++ SynetecAssessmentApi/Services/BonusPoolService.cs
@@ -2,6 +2,7 @@
 using SynetecAssessmentApi.Domain;
 using SynetecAssessmentApi.Dtos;
 using SynetecAssessmentApi.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,12 @@ namespace SynetecAssessmentApi.Services
 {
     public class BonusPoolService
     {
-        private readonly AppDbContext _dbContext;
+        protected AppDbContext _dbContext;
+
+        protected virtual void setDbContext(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         public BonusPoolService()
         {
@@ -48,6 +54,24 @@ namespace SynetecAssessmentApi.Services
             return result;
         }
 
+        public bool isBonusValid(CalculateBonusDto request)
+        {
+            bool isRequestValid = false;
+
+            if(request != null)
+            {
+                isRequestValid = true;
+            }
+
+            return isRequestValid;
+        }
+
+        /// <summary>
+        /// Calculates Bonus for the selected employee
+        /// </summary>
+        /// <param name="bonusPoolAmount"></param>
+        /// <param name="selectedEmployeeId"></param>
+        /// <returns></returns>
         public async Task<BonusPoolCalculatorResultDto> CalculateAsync(int bonusPoolAmount, int selectedEmployeeId)
         {
             //load the details of the selected employee using the Id
@@ -55,8 +79,19 @@ namespace SynetecAssessmentApi.Services
                 .Include(e => e.Department)
                 .FirstOrDefaultAsync(item => item.Id == selectedEmployeeId);
 
+            if(employee == null)
+            {
+                throw new ArgumentException("Employee with ID " + selectedEmployeeId.ToString() + " not found");
+            }
+
             //get the total salary budget for the company
             int totalSalary = (int)_dbContext.Employees.Sum(item => item.Salary);
+
+            //Precaution: In case the database is not seeded or seeded with negative values
+            if(totalSalary <= 0)
+            {
+                throw new Exception("Sum of salaries is incorrect");
+            }
 
             //calculate the bonus allocation for the employee
             decimal bonusPercentage = (decimal)employee.Salary / (decimal)totalSalary;
